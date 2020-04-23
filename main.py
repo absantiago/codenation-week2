620a52856501dd7ba5f5094734e65b518e8df0a3
Arthur brandao <arthur.santiago@ice.ufjf.br>
2020-04-23 15:28:49 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Desafio 1
+# 
+# Para esse desafio, vamos trabalhar com o data set [Black Friday](https://www.kaggle.com/mehdidag/black-friday), que reúne dados sobre transações de compras em uma loja de varejo.
+# 
+# Vamos utilizá-lo para praticar a exploração de data sets utilizando pandas. Você pode fazer toda análise neste mesmo notebook, mas as resposta devem estar nos locais indicados.
+# 
+# > Obs.: Por favor, não modifique o nome das funções de resposta.
+
+# ## _Set up_ da análise
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+black_friday = pd.read_csv("black_friday.csv")
+
+
+# ## Inicie sua análise a partir daqui
+
+# In[54]:
+
+
+black_friday.info()
+
+
+# In[59]:
+
+
+black_friday.loc[black_friday['Gender'] == 'F' ].loc[black_friday['Age'] ==  '26-35'].info()
+
+
+# In[64]:
+
+
+len(black_friday['User_ID'].unique())
+
+
+# In[91]:
+
+
+black_friday.isna().sum()
+
+
+# In[92]:
+
+
+black_friday.isnull().sum()
+
+
+# In[93]:
+
+
+p2 = black_friday.loc[black_friday['Product_Category_2'].isna()]
+p2['Product_Category_3'].sum()
+#todos registros com Product_category_2 nulos também tem o Product_category_3 Nulos
+percent = 373299/537577
+percent
+
+
+# In[97]:
+
+
+black_friday['Product_Category_3'].mode()
+
+
+# In[121]:
+
+
+from sklearn import preprocessing
+purchase = black_friday['Purchase'].values.reshape(-1,1)
+min_max_scaler = preprocessing.MinMaxScaler()
+purchase_scaled = min_max_scaler.fit_transform(purchase)
+purchase_scaled.mean()
+
+
+# In[139]:
+
+
+from sklearn.preprocessing import StandardScaler
+purchase = black_friday['Purchase'].values.reshape(-1,1)
+standarlizer = StandardScaler()
+purchase_standarlized = standarlizer.fit_transform(purchase)
+purchase_standarlized
+
+i=0
+for purchase in purchase_standarlized:
+    if(purchase>-1 and purchase <1):
+        i=i+1
+
+
+# ## Questão 1
+# 
+# Quantas observações e quantas colunas há no dataset? Responda no formato de uma tuple `(n_observacoes, n_colunas)`.
+
+# In[4]:
+
+
+def q1():
+    # Retorne aqui o resultado da questão 1.
+    return(537577,12)
+
+
+# ## Questão 2
+# 
+# Há quantas mulheres com idade entre 26 e 35 anos no dataset? Responda como um único escalar.
+
+# In[5]:
+
+
+def q2():
+    # Retorne aqui o resultado da questão 2.
+    return 49348
+
+
+# ## Questão 3
+# 
+# Quantos usuários únicos há no dataset? Responda como um único escalar.
+
+# In[6]:
+
+
+def q3():
+    # Retorne aqui o resultado da questão 3.
+    return(5891)
+
+
+# ## Questão 4
+# 
+# Quantos tipos de dados diferentes existem no dataset? Responda como um único escalar.
+
+# In[7]:
+
+
+def q4():
+    # Retorne aqui o resultado da questão 4.
+    return(3)
+
+
+# ## Questão 5
+# 
+# Qual porcentagem dos registros possui ao menos um valor null (`None`, `ǸaN` etc)? Responda como um único escalar entre 0 e 1.
+
+# In[8]:
+
+
+def q5():
+    # Retorne aqui o resultado da questão 5.
+    return(0.69)
+
+
+# ## Questão 6
+# 
+# Quantos valores null existem na variável (coluna) com o maior número de null? Responda como um único escalar.
+
+# In[9]:
+
+
+def q6():
+    # Retorne aqui o resultado da questão 6.
+    return(373299)
+
+
+# ## Questão 7
+# 
+# Qual o valor mais frequente (sem contar nulls) em `Product_Category_3`? Responda como um único escalar.
+
+# In[10]:
+
+
+def q7():
+    # Retorne aqui o resultado da questão 7.
+    return(16.0)
+
+
+# ## Questão 8
+# 
+# Qual a nova média da variável (coluna) `Purchase` após sua normalização? Responda como um único escalar.
+
+# In[11]:
+
+
+def q8():
+    # Retorne aqui o resultado da questão 8.
+    return(0.3847)
+
+
+# ## Questão 9
+# 
+# Quantas ocorrências entre -1 e 1 inclusive existem da variáel `Purchase` após sua padronização? Responda como um único escalar.
+
+# In[12]:
+
+
+def q9():
+    # Retorne aqui o resultado da questão 9.
+    return (348631)
+
+
+# ## Questão 10
+# 
+# Podemos afirmar que se uma observação é null em `Product_Category_2` ela também o é em `Product_Category_3`? Responda com um bool (`True`, `False`).
+
+# In[13]:
+
+
+def q10():
+    # Retorne aqui o resultado da questão 10.
+    return True
+
